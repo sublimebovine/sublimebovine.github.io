7b9fa266b01b2b5612a0f8af17150d33ce3fe44c
sublimebovine <87881310+sublimebovine@users.noreply.github.com>
2025-04-19 21:20:59 -0400

    Update zaphod.sql

--- zaphod.sql
+++ zaphod.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `username` varchar(20) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `premium` char(1) DEFAULT NULL,
   `confidence` int DEFAULT NULL
