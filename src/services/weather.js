68c6ff354dca942ef46b2919bb5b36fa40da2659
Abhinav Bollu <abhinavbollu921@gmail.com>
2025-04-23 21:58:42 -0400

    added weather service

---
+++ src/services/weather.js
@@ -0,0 +1,18 @@
+const WEATHER_API_KEY = ac5daa5456cd2baab84b202651b13f05;
+const WEATHER_API_BASE_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const getWeather = async (lat, lon) => {
+    try {
+        const response = await fetch(`${WEATHER_API_BASE_URL}/weather?lat=${lat}&lon=${lon}&units=metric&appid=${WEATHER_API_KEY}`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch weather data');
+        }
+        
+        return await response.json();
+    }
+    catch (error) {
+        console.error('Error fetching weather data:', error);
+        throw error;
+    }
+};
+
